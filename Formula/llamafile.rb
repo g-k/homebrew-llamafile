e4c2921607ba353e9c14fb08ac9634d527457dd6
Greg Guthe <greg.guthe@gmail.com>
2024-12-30 20:47:56 -0500

    Update llamafile to 0.8.17

--- Formula/llamafile.rb
+++ Formula/llamafile.rb
@@ -14,7 +14,7 @@ class Llamafile < Formula
 
   if build.with? "extras"
     url "https://github.com/Mozilla-Ocho/llamafile/releases/download/0.8.17/llamafile-0.8.17.zip"
-    sha256 "1041e05b2c254674e03c66052b1a6cf646e8b15ebd29a195c77fed92cac60d6b"
+    sha256 "8567e1c50aa509c4e63844ddbb934b096fa7cb71e52b75a38392d7caa01cae2b"
   else
     url "https://github.com/Mozilla-Ocho/llamafile/releases/download/0.8.17/llamafile-0.8.17"
     sha256 "1041e05b2c254674e03c66052b1a6cf646e8b15ebd29a195c77fed92cac60d6b"
